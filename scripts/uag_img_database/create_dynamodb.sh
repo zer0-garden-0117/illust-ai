2899a0cda011903642d15f86aa1ac124f76622bf
zer0 <zer0.garden.0117@gmail.com>
2024-07-13 19:43:07 +0900

    usersテーブルの追加、gsiやApiWorks、ApiImgsの見直し

--- scripts/uag_img_database/create_dynamodb.sh
+++ scripts/uag_img_database/create_dynamodb.sh
@@ -6,21 +6,6 @@ ENDPOINT_URL=http://localhost:10000
 # AWS CLI プロファイル
 AWS_PROFILE=default
 
-# IDを生成する関数
-generate_id() {
-    local counter_name=$1
-    local current_value=$(aws dynamodb update-item \
-        --table-name counters \
-        --key "{\"counter_name\": {\"S\": \"$counter_name\"}}" \
-        --update-expression "SET counter_value = if_not_exists(counter_value, :start) + :inc" \
-        --expression-attribute-values '{":inc": {"N": "1"}, ":start": {"N": "0"}}' \
-        --return-values "UPDATED_NEW" \
-        --endpoint-url $ENDPOINT_URL \
-        --query "Attributes.counter_value.N" \
-        --output text)
-    echo $current_value
-}
-
 # counters テーブルの作成
 aws dynamodb create-table \
     --table-name counters \
@@ -56,16 +41,10 @@ aws dynamodb create-table \
 # tags テーブルの作成
 aws dynamodb create-table \
     --table-name tags \
-    --attribute-definitions AttributeName=id,AttributeType=S AttributeName=name,AttributeType=S \
-    --key-schema AttributeName=id,KeyType=HASH \
-    --global-secondary-indexes "[
-        {
-            \"IndexName\": \"name-index\",
-            \"KeySchema\": [{\"AttributeName\":\"name\",\"KeyType\":\"HASH\"}],
-            \"Projection\": {\"ProjectionType\":\"ALL\"},
-            \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 5, \"WriteCapacityUnits\": 5}
-        }
-    ]" \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+    --key-schema \
+        AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --endpoint-url $ENDPOINT_URL
 
@@ -74,8 +53,63 @@ aws dynamodb create-table \
     --table-name work_tags \
     --attribute-definitions AttributeName=work_id,AttributeType=S AttributeName=tag_id,AttributeType=S \
     --key-schema AttributeName=work_id,KeyType=HASH AttributeName=tag_id,KeyType=RANGE \
+    --global-secondary-indexes "[
+        {
+            \"IndexName\": \"tag_id-index\",
+            \"KeySchema\": [{\"AttributeName\":\"tag_id\",\"KeyType\":\"HASH\"}],
+            \"Projection\": {\"ProjectionType\":\"ALL\"},
+            \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 5, \"WriteCapacityUnits\": 5}
+        }
+    ]" \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --endpoint-url $ENDPOINT_URL
+
+# users テーブルの作成
+aws dynamodb create-table \
+    --table-name users \
+    --attribute-definitions AttributeName=userId,AttributeType=S \
+    --key-schema AttributeName=userId,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --endpoint-url $ENDPOINT_URL
+
+# user_likes テーブルの作成
+aws dynamodb create-table \
+    --table-name user_likes \
+    --attribute-definitions AttributeName=userId,AttributeType=S AttributeName=workId,AttributeType=S \
+    --key-schema AttributeName=userId,KeyType=HASH AttributeName=workId,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --endpoint-url $ENDPOINT_URL
+
+# user_downloads テーブルの作成
+aws dynamodb create-table \
+    --table-name user_downloads \
+    --attribute-definitions AttributeName=userId,AttributeType=S AttributeName=workId,AttributeType=S \
+    --key-schema AttributeName=userId,KeyType=HASH AttributeName=workId,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --endpoint-url $ENDPOINT_URL
+
+# user_views テーブルの作成
+aws dynamodb create-table \
+    --table-name user_views \
+    --attribute-definitions AttributeName=userId,AttributeType=S AttributeName=workId,AttributeType=S \
+    --key-schema AttributeName=userId,KeyType=HASH AttributeName=workId,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --endpoint-url $ENDPOINT_URL
+
+# IDを生成する関数
+generate_id() {
+    local counter_name=$1
+    local current_value=$(aws dynamodb update-item \
+        --table-name counters \
+        --key "{\"counter_name\": {\"S\": \"$counter_name\"}}" \
+        --update-expression "SET counter_value = if_not_exists(counter_value, :start) + :inc" \
+        --expression-attribute-values '{":inc": {"N": "1"}, ":start": {"N": "0"}}' \
+        --return-values "UPDATED_NEW" \
+        --endpoint-url $ENDPOINT_URL \
+        --query "Attributes.counter_value.N" \
+        --output text)
+    echo $current_value
+}
 
 # works テーブルにテストデータを追加
 work_ids=()
@@ -85,7 +119,7 @@ do
     work_ids+=($work_id)
     aws dynamodb put-item \
         --table-name works \
-        --item "{\"id\": {\"S\": \"$work_id\"}, \"title\": {\"S\": \"Sample Work $i\"}, \"title_image_url\": {\"S\": \"http://example.com/title_image_$i.jpg\"}, \"creator\": {\"S\": \"Sample Creator $i\"}, \"category\": {\"S\": \"Art\"}, \"subject\": {\"S\": \"Abstract\"}, \"language\": {\"S\": \"English\"}, \"created_at\": {\"S\": \"2024-07-08T00:00:00Z\"}, \"updated_at\": {\"S\": \"2024-07-08T00:00:00Z\"}, \"page_count\": {\"N\": \"100\"}, \"likes\": {\"N\": \"10\"}, \"downloads\": {\"N\": \"5\"}}" \
+        --item "{\"id\": {\"S\": \"$work_id\"}, \"mainTitle\": {\"S\": \"Sample Work $i\"}, \"subTitle\": {\"S\": \"Sample Subtitle $i\"}, \"description\": {\"S\": \"Sample Description $i\"}, \"workFormat\": {\"S\": \"CG/コミック/ゲーム\"}, \"topicGenre\": {\"S\": \"オリジナル/二次創作/パロディ\"}, \"creator\": {\"S\": \"Sample Creator $i\"}, \"pages\": {\"S\": \"30\"}, \"size\": {\"S\": \"30MB\"}, \"language\": {\"S\": \"English\"}, \"likes\": {\"N\": \"0\"}, \"downloads\": {\"N\": \"0\"}, \"titleImageUrl\": {\"S\": \"http://example.com/title_image_$i.jpg\"}, \"createdAt\": {\"S\": \"2024-07-08T00:00:00Z\"}, \"updatedAt\": {\"S\": \"2024-07-08T00:00:00Z\"}}" \
         --endpoint-url $ENDPOINT_URL
 
     # images テーブルにテストデータを追加
@@ -94,7 +128,7 @@ do
         img_id=$(generate_id "img_id")
         aws dynamodb put-item \
             --table-name images \
-            --item "{\"id\": {\"S\": \"$img_id\"}, \"work_id\": {\"S\": \"$work_id\"}, \"s3_url\": {\"S\": \"http://example.com/image${i}_${j}.jpg\"}}" \
+            --item "{\"id\": {\"S\": \"$img_id\"}, \"work_id\": {\"S\": \"$work_id\"}, \"s3_url\": {\"S\": \"http://example.com/image${i}_${j}.jpg\"}, \"pageNumber\": {\"N\": \"$j\"}}" \
             --endpoint-url $ENDPOINT_URL
     done
 done
@@ -119,6 +153,56 @@ do
         aws dynamodb put-item \
             --table-name work_tags \
             --item "{\"work_id\": {\"S\": \"$work_id\"}, \"tag_id\": {\"S\": \"$tag_id\"}}" \
+            --endpoint-url $ENDPOINT_URL
+    done
+done
+
+# users テーブルにテストデータを追加
+user_ids=()
+user_roles=("admin" "editor" "viewer")
+for i in "${!user_roles[@]}"
+do
+    user_id=$(generate_id "user_id")
+    user_ids+=($user_id)
+    user_role=${user_roles[$i]}
+    aws dynamodb put-item \
+        --table-name users \
+        --item "{\"userId\": {\"S\": \"$user_id\"}, \"userRole\": {\"S\": \"$user_role\"}}" \
+        --endpoint-url $ENDPOINT_URL
+done
+
+# user_likes テーブルにテストデータを追加
+for user_id in "${user_ids[@]}"
+do
+    for work_id in "${work_ids[@]}"
+    do
+        aws dynamodb put-item \
+            --table-name user_likes \
+            --item "{\"userId\": {\"S\": \"$user_id\"}, \"workId\": {\"S\": \"$work_id\"}}" \
+            --endpoint-url $ENDPOINT_URL
+    done
+done
+
+# user_downloads テーブルにテストデータを追加
+for user_id in "${user_ids[@]}"
+do
+    for work_id in "${work_ids[@]}"
+    do
+        aws dynamodb put-item \
+            --table-name user_downloads \
+            --item "{\"userId\": {\"S\": \"$user_id\"}, \"workId\": {\"S\": \"$work_id\"}}" \
+            --endpoint-url $ENDPOINT_URL
+    done
+done
+
+# user_views テーブルにテストデータを追加
+for user_id in "${user_ids[@]}"
+do
+    for work_id in "${work_ids[@]}"
+    do
+        aws dynamodb put-item \
+            --table-name user_views \
+            --item "{\"userId\": {\"S\": \"$user_id\"}, \"workId\": {\"S\": \"$work_id\"}}" \
             --endpoint-url $ENDPOINT_URL
     done
 done
--- scripts/uag_img_database/drop.sh
+++ scripts/uag_img_database/drop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # テーブル名のリスト
-tables=("works" "images" "tags" "work_tags" "counters")
+tables=("works" "images" "tags" "work_tags" "counters" "users" "user_likes" "user_downloads" "user_views")
 
 # 各テーブルを削除
 for table in "${tables[@]}"
 do
-    aws dynamodb delete-table \
-        --table-name "$table" \
-        --endpoint-url http://localhost:10000 \
-        --no-cli-pager
+    echo "Deleting table: $table"
+    command="aws dynamodb delete-table --table-name $table --endpoint-url http://localhost:10000 --no-cli-pager"
+    echo "Running command: $command"
+    $command
 done
