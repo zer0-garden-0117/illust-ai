e4562e3e3404eced9184acbfe1a6aadf7003d590
zer0 <zer0.garden.0117@gmail.com>
2024-09-22 17:02:17 +0900

    postのpageのSR化、postのGridの表示の仕方設定

--- uag-frontend/uag-ui/src/app/[locale]/posts/[id]/page.tsx
+++ uag-frontend/uag-ui/src/app/[locale]/posts/[id]/page.tsx
@@ -1,10 +1,14 @@
+'use client';
+
+import Post from '@/components/Content/Post/Post';
 import { Text } from '@mantine/core';
 
-export default async function PostPage({ params }: { params: { id: string } }) {
+export default function PostPage({ params }: { params: { id: string } }) {
 
   return (
     <div>
       <Text>{params.id}</Text>
+      <Post />
     </div>
   );
 }
--- uag-frontend/uag-ui/src/components/Content/Post/Post.view.tsx
+++ uag-frontend/uag-ui/src/components/Content/Post/Post.view.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import React from 'react';
 import { Button, Fieldset, Grid } from '@mantine/core';
 import { memo } from 'react';
 import { useTranslations } from "next-intl";
+import WorkRegistrationForm from '../WorkRegistrationForm/WorkRegistrationForm';
 
 type PostViewProps = {
   onOpen: () => void;
@@ -15,10 +18,10 @@ export const PostView = memo(function PostViewComponent({
   return (
     <Fieldset legend={t('')}>
       <Grid>
-        <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
+        <Grid.Col span={{ base: 12, sm: 6, lg: 6 }}>
           1
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
+        <Grid.Col span={{ base: 12, sm: 6, lg: 6 }}>
           2
         </Grid.Col>
       </Grid>
