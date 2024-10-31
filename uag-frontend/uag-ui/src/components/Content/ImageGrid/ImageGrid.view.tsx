27cf789ccc723ae446327c1b7ee439e0fbe9882f
zer0 <zer0.garden.0117@gmail.com>
2024-11-01 02:55:38 +0900

    ログイン直後の画像のチラつきを修正

--- uag-frontend/uag-ui/src/components/Content/ImageGrid/ImageGrid.view.tsx
+++ uag-frontend/uag-ui/src/components/Content/ImageGrid/ImageGrid.view.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { AspectRatio, Card, SimpleGrid, Text, Image as MantineImage, Pagination, ActionIcon, Rating, Group, Fieldset, Transition } from '@mantine/core';
+import { AspectRatio, Card, SimpleGrid, Text, Pagination, ActionIcon, Rating, Group, Fieldset } from '@mantine/core';
 import { memo } from 'react';
 import { useTranslations } from "next-intl";
 import { useRouter } from 'next/navigation';
@@ -33,6 +33,148 @@ type ImageGridViewProps = {
   isAuthenticated: boolean;
 };
 
+const CustomImage = ({ src, alt, index }: { src: string; alt: string; index: number }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const imgRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (imgRef.current) {
+      observer.observe(imgRef.current);
+    }
+
+    return () => {
+      if (imgRef.current) {
+        observer.unobserve(imgRef.current);
+      }
+    };
+  }, []);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+
+  const shouldDisplay = isVisible && isLoaded;
+
+  return (
+    <div ref={imgRef} style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <img
+        src={src}
+        alt={alt}
+        onLoad={handleImageLoad}
+        style={{
+          width: '100%',
+          height: '100%',
+          opacity: shouldDisplay ? 1 : 0,
+          transform: shouldDisplay ? 'translateY(0)' : 'translateY(20px)',
+          transition: `opacity 0.3s ease-in-out ${index * 0.1}s, transform 0.3s ease-in-out ${index * 0.1}s`,
+        }}
+      />
+      {!shouldDisplay && (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'transparent',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+// Memoized CustomImage
+const MemoizedImage = memo(CustomImage, (prevProps, nextProps) => prevProps.src === nextProps.src);
+
+const MemoizedCard = memo(
+  ({ imageData, index, onRateChange, onLikeChange, isAuthenticated, router, setLoginModalOpen }: { imageData: ImageData, index: number, onRateChange: (workId: number, value: number) => void, onLikeChange: (workId: number) => void, isAuthenticated: boolean, router: ReturnType<typeof useRouter>, setLoginModalOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
+    const [localIsLiked, setLocalIsLiked] = useState(imageData.isLiked);
+    const [localRating, setLocalRating] = useState(imageData.rating);
+
+    useEffect(() => {
+      setLocalRating(imageData.rating);
+    }, [imageData.rating]);
+
+    useEffect(() => {
+      setLocalIsLiked(imageData.isLiked);
+    }, [imageData.isLiked]);
+
+    const handleLikeClick = () => {
+      if (!isAuthenticated) {
+        setLoginModalOpen(true);
+        return;
+      }
+
+      setLocalIsLiked(!localIsLiked);
+      onLikeChange(imageData.workId);
+    };
+
+    const handleRateClick = (rating: number) => {
+      if (!isAuthenticated) {
+        setLoginModalOpen(true);
+        return;
+      }
+      setLocalRating(rating);
+      onRateChange(imageData.workId, rating);
+    };
+
+    const handleImageClick = () => {
+      router.push(`/works/${imageData.workId}`);
+    };
+
+    return (
+      <Card key={imageData.workId} p="md" radius="md" className={classes.card}>
+        <AspectRatio ratio={1 / Math.sqrt(2)}>
+          <div onClick={handleImageClick} style={{ cursor: 'pointer' }}>
+            <MemoizedImage
+              src={imageData.thumbnailImage}
+              alt={imageData.mainTitle || "Image without title"}
+              index={index}
+            />
+          </div>
+        </AspectRatio>
+        <Group>
+          <Text className={classes.title} mt={5}>
+            {imageData.mainTitle}
+          </Text>
+        </Group>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginTop: '12px' }}>
+          <Rating value={localRating} onChange={handleRateClick} />
+          <ActionIcon
+            variant="transparent"
+            color="gray"
+            style={{
+              color: localIsLiked ? 'red' : 'gray',
+              transition: 'color 0.3s ease',
+              padding: 0,
+              marginLeft: '8px',
+            }}
+            onClick={handleLikeClick}
+          >
+            <RiHeartAdd2Line />
+          </ActionIcon>
+        </div>
+      </Card>
+    );
+  },
+  (prevProps, nextProps) => prevProps.imageData.rating === nextProps.imageData.rating && prevProps.imageData.isLiked === nextProps.imageData.isLiked
+);
+MemoizedCard.displayName = 'MemoizedCard';
+
 export const ImageGridView = memo(function ImageGridViewComponent({
   title,
   isViewCount,
@@ -49,150 +191,6 @@ export const ImageGridView = memo(function ImageGridViewComponent({
   const t = useTranslations("");
   const router = useRouter();
   const [loginModalOpen, setLoginModalOpen] = useState(false);
-  const [allPlaceholdersVisible, setAllPlaceholdersVisible] = useState(false);
-  const visibleStatus = useRef(Array(imageData.length).fill(false));
-
-  const checkAllPlaceholdersVisible = () => {
-    if (visibleStatus.current.every(status => status === true)) {
-      setAllPlaceholdersVisible(true);
-    }
-  };
-
-  const CustomImage = ({ src, alt, index }: { src: string; alt: string; index: number }) => {
-    const [isVisible, setIsVisible] = useState(false); // IntersectionObserverの可視状態
-    const [isLoaded, setIsLoaded] = useState(false);   // onLoadのロード状態
-    const imgRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              setIsVisible(true); // ビューポート内に入ったら可視状態にする
-            }
-          });
-        },
-        { threshold: 0.1 } // 要素の10%が表示されたらトリガー
-      );
-
-      if (imgRef.current) {
-        observer.observe(imgRef.current);
-      }
-
-      return () => {
-        if (imgRef.current) {
-          observer.unobserve(imgRef.current);
-        }
-      };
-    }, []);
-
-    const handleImageLoad = () => {
-      setIsLoaded(true); // 画像がロードされたらロード状態にする
-    };
-
-    const shouldDisplay = isVisible && isLoaded; // 両方の条件が満たされたら表示
-
-    return (
-      <div ref={imgRef} style={{ position: 'relative', width: '100%', height: '100%' }}>
-        {/* 両方の条件が満たされたらフェードインと"fade-up"トランジションを開始 */}
-        <img
-          src={src}
-          alt={alt}
-          onLoad={handleImageLoad} // 画像のロード完了を確認
-          style={{
-            width: '100%',
-            height: '100%',
-            opacity: shouldDisplay ? 1 : 0, // フェードイン
-            transform: shouldDisplay ? 'translateY(0)' : 'translateY(20px)', // 下から上に移動
-            transition: `opacity 0.3s ease-in-out ${index * 0.1}s, transform 0.3s ease-in-out ${index * 0.1}s`, // 遅延をindexに基づいて設定
-          }}
-        />
-
-        {/* プレースホルダー：表示するまでの間のスペース確保用 */}
-        {!shouldDisplay && (
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              backgroundColor: 'transparent',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-            }}
-          />
-        )}
-      </div>
-    );
-  };
-
-  const MemoizedCard = memo(
-    ({ imageData, index, onRateChange, onLikeChange }: { imageData: ImageData, index: number, onRateChange: (workId: number, value: number) => void, onLikeChange: (workId: number) => void }) => {
-      const [localIsLiked, setLocalIsLiked] = useState(imageData.isLiked);
-      const [localRating, setLocalRating] = useState(imageData.rating);
-
-      const handleLikeClick = () => {
-        if (!isAuthenticated) {
-          setLoginModalOpen(true);
-          return;
-        }
-
-        setLocalIsLiked(!localIsLiked);
-        onLikeChange(imageData.workId);
-      };
-
-      const handleRateClick = (rating: number) => {
-        if (!isAuthenticated) {
-          setLoginModalOpen(true);
-          return;
-        }
-        setLocalRating(rating);
-        onRateChange(imageData.workId, rating);
-      };
-
-      const handleImageClick = () => {
-        router.push(`/works/${imageData.workId}`);
-      };
-
-      return (
-        <Card
-          key={imageData.workId} p="md" radius="md" className={classes.card}
-        >
-          <AspectRatio ratio={1 / Math.sqrt(2)}>
-            <div onClick={handleImageClick} style={{ cursor: 'pointer' }}>
-              <CustomImage
-                src={imageData.thumbnailImage}
-                alt={imageData.mainTitle || "Image without title"}
-                index={index}
-              />
-            </div>
-          </AspectRatio>
-          <Group>
-            <Text className={classes.title} mt={5}>
-              {imageData.mainTitle}
-            </Text>
-          </Group>
-          <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginTop: '12px' }}>
-            <Rating value={localRating} onChange={handleRateClick} />
-            <ActionIcon
-              variant="transparent"
-              color="gray"
-              style={{
-                color: localIsLiked ? 'red' : 'gray',
-                transition: 'color 0.3s ease',
-                padding: 0,
-                marginLeft: '8px',
-              }}
-              onClick={handleLikeClick}
-            >
-              <RiHeartAdd2Line />
-            </ActionIcon>
-          </div>
-        </Card>
-      );
-    },
-    (prevProps, nextProps) => prevProps.imageData.rating === nextProps.imageData.rating && prevProps.imageData.isLiked === nextProps.imageData.isLiked
-  );
-  MemoizedCard.displayName = 'ImageGridViewComponent';
 
   const cards = imageData.map((data, index) => (
     <MemoizedCard
@@ -201,6 +199,9 @@ export const ImageGridView = memo(function ImageGridViewComponent({
       index={index}
       onRateChange={onRateChange}
       onLikeChange={onLikeChange}
+      isAuthenticated={isAuthenticated}
+      router={router}
+      setLoginModalOpen={setLoginModalOpen}
     />
   ));
 
