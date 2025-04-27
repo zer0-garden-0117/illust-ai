762038e0a6d7b1747dd23e659ba89baae15c356a
zer0 <zer0@zer0noMac-mini.local>
2025-04-27 22:52:48 +0900

    githubのリンクデザイン修正

--- asb-frontend/src/components/Header/GithubLink/GithubLink.tsx
+++ asb-frontend/src/components/Header/GithubLink/GithubLink.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { FaGithubAlt } from "react-icons/fa";
-import { Box } from '@mantine/core';
+import { Box, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { BsGithub } from "react-icons/bs";
 import { useRouter } from "next/navigation";
 
 export const GithubLink: React.FC = () => {
   const router = useRouter();
+  const { colorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === 'dark';
   
   const onIconClick = async () => {
     router.push('/');
   };
 
   return (
-    <Box mt={3}>
-      <FaGithubAlt
-        size="1.6rem"
+    <Box mt={2}>
+      <ActionIcon
+        variant="outline"
+        color={isDark ? "var(--mantine-color-gray-5)" : "var(--mantine-color-gray-8)"} 
+        radius='md'
+        styles={{
+          root: {
+            borderColor: isDark 
+              ? "var(--mantine-color-gray-8)"
+              : "var(--mantine-color-gray-5)",
+          }
+        }}
+      >
+      <BsGithub
+        size="1.3rem"
         height="32"
         style={{ 
             cursor: 'pointer',
@@ -21,6 +35,7 @@ export const GithubLink: React.FC = () => {
           }}
         onClick={onIconClick}
       />
+      </ActionIcon>
     </Box>
   );
 };
