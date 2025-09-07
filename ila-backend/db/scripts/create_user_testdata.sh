6eecd5c58727b3c8cf614fabf852929dde1e72ae
zer0 <zer0@zer0noMac-mini.local>
2025-09-07 23:06:09 +0900

    デフォルトのアイコン、カバー画像設定

--- ila-backend/db/scripts/create_user_testdata.sh
+++ ila-backend/db/scripts/create_user_testdata.sh
@@ -46,8 +46,8 @@ put_item user '{
   "updatedAt": {"S": "2024-01-15T10:30:00Z"},
   "userName": {"S": "testUserName"},
   "userProfile": {"S": "testUserProfile"},
-  "profileImageUrl": {"S": "https://example.com/images/user001.jpg"},
-  "coverImageUrl": {"S": "https://example.com/images/user001.jpg"},
+  "profileImageUrl": {"S": ""},
+  "coverImageUrl": {"S": ""},
   "createdAt": {"S": "2023-05-10T14:20:00Z"}
 }'
 
@@ -57,8 +57,8 @@ put_item user '{
   "updatedAt": {"S": "2024-01-15T10:30:00Z"},
   "userName": {"S": "testUserName"},
   "userProfile": {"S": "testUserProfile"},
-  "profileImageUrl": {"S": "https://example.com/images/user001.jpg"},
-  "coverImageUrl": {"S": "https://example.com/images/user001.jpg"},
+  "profileImageUrl": {"S": ""},
+  "coverImageUrl": {"S": ""},
   "createdAt": {"S": "2023-05-10T14:20:00Z"}
 }'
 
@@ -68,8 +68,8 @@ put_item user '{
   "updatedAt": {"S": "2024-01-15T10:30:00Z"},
   "userName": {"S": "testUserName"},
   "userProfile": {"S": "testUserProfile"},
-  "profileImageUrl": {"S": "https://example.com/images/user001.jpg"},
-  "coverImageUrl": {"S": "https://example.com/images/user001.jpg"},
+  "profileImageUrl": {"S": ""},
+  "coverImageUrl": {"S": ""},
   "createdAt": {"S": "2023-05-10T14:20:00Z"}
 }'
 
--- ila-frontend/src/components/Content/UserInfo/UserInfo.view.tsx
+++ ila-frontend/src/components/Content/UserInfo/UserInfo.view.tsx
@@ -19,12 +19,17 @@ export const UserInfoView = memo(function WorkViewComponent({
       <Card.Section
         h={140}
         style={{
-          backgroundImage: userData?.profileImageUrl,
+          backgroundImage: userData?.profileImageUrl 
+            ? `url(${userData.profileImageUrl})` 
+            : 'url(/cover.png)',
           // backgroundImage: 'url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80)',
         }}
       />
       <Avatar
-        src={userData?.profileImageUrl}
+        src={
+          userData?.profileImageUrl
+            ? userData.profileImageUrl
+            : '/icon.png'}
         // src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-9.png"
         size={80}
         radius={80}
