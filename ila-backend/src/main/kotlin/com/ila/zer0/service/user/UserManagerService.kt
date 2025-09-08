4c24dc8032824a10e1552e995e9aad70001c9392
zer0 <zer0@zer0noMac-mini.local>
2025-09-08 22:26:07 +0900

    デフォルトのデザイン修正

--- ila-backend/src/main/kotlin/com/ila/zer0/service/user/UserManagerService.kt
+++ ila-backend/src/main/kotlin/com/ila/zer0/service/user/UserManagerService.kt
@@ -34,8 +34,8 @@ class UserManagerService(
         newUser.customUserId = uuidService.generateUuid()
         newUser.userName = userId
         newUser.userProfile = ""
-        newUser.profileImageUrl = "https://ila-backend.s3.us-east-2.amazonaws.com/icon.png"
-        newUser.coverImageUrl = "https://ila-backend.s3.us-east-2.amazonaws.com/cover.png"
+        newUser.profileImageUrl = "https://ila-backend.s3.us-east-2.amazonaws.com/icon2.png"
+        newUser.coverImageUrl = "https://ila-backend.s3.us-east-2.amazonaws.com/cover2.png"
         newUser.createdAt = Instant.now()
         newUser.updatedAt = Instant.now()
         return userService.registerUser(newUser)
