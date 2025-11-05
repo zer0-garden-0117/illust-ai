8b58dbc76dcb489a32c6e6f41784f5e9b7dc18a5
zer0 <zer0.garden.0117@gmail.com>
2025-11-05 21:20:25 +0900

    TopCardの修正

--- ila-frontend/src/components/Content/TopCards/TopCards.hook.ts
+++ ila-frontend/src/components/Content/TopCards/TopCards.hook.ts
@@ -3,16 +3,31 @@ import { useFirebaseAuthContext } from "@/providers/auth/firebaseAuthProvider";
 import { useWorksGetById } from "@/apis/openapi/works/useWorksGetById";
 import { useState } from "react";
 import { useWorksUpdate } from "@/apis/openapi/works/useWorksUpdate";
+import { usePublicWorksGet } from "@/apis/openapi/works/usePublicWorksGet";
 
 export const useTopCards = () => {
   const { user, getIdTokenLatest } = useFirebaseAuthContext();
   const { trigger: triggerPost } = useWorksUpdate();
   const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isPosted, setIsPosted] = useState(false);
+  const [illustNum, setIllustNum] = useState(4);
+  const worksFilterType = 'new';
+  const { data: worksData, mutate: updateWorks } = usePublicWorksGet({
+      offset: 0,
+      limit: illustNum,
+      worksFilterType,
+  });
+
+  const handleMoreClick = async () => {
+    setIsSubmitting(true);
+    setIllustNum(illustNum + 4);
+    await updateWorks();
+    setIsSubmitting(false);
+  }
 
   return {
+    worksData,
     isSubmitting,
-    isPosted,
+    handleMoreClick
   };
 };
--- ila-frontend/src/components/Content/TopCards/TopCards.view.tsx
+++ ila-frontend/src/components/Content/TopCards/TopCards.view.tsx
@@ -3,27 +3,47 @@
 import React, { memo } from 'react';
 import { Group, Card, Grid, Image, Textarea, AspectRatio, Center, Button, Loader, Space, SimpleGrid } from '@mantine/core';
 import { IconCheck, IconPencil } from '@tabler/icons-react';
+import { PublicWorksGetResult } from '@/apis/openapi/works/usePublicWorksGet';
+import { ImageCard } from '../ImageCard/ImageCard';
 
 type TopCardsViewProps = {
+  worksData: PublicWorksGetResult | undefined;
   isSubmitting: boolean;
-  isPosted: boolean;
+  handleMoreClick: () => Promise<void>;
 };
 
 export const TopCardsView = memo(function WorkViewComponent({
+  worksData,
   isSubmitting,
-  isPosted
+  handleMoreClick
 }: TopCardsViewProps): JSX.Element {
-  console.log('isSubmitting:', isSubmitting);
-  console.log('isPosted:', isPosted);
   return (
     <>
       <Card withBorder padding="md" radius="md">
 
+        {/* newの作品 */}
         <SimpleGrid cols={{ base: 2, sm: 3, md: 4, lg: 4, xl: 4 }} spacing={{ base: 20 }}>
-
-          
+        {!worksData &&
+          Array.from({ length: 12 }).map((_, idx) => (
+            <ImageCard key={idx} data={{}} index={idx} />
+          ))
+        }
+          {worksData?.works?.map((work, idx) => (
+            <ImageCard key={work.apiWork?.workId} data={work} index={idx} />
+          ))}
         </SimpleGrid>
+        <Space h="md" />
 
+        {/* もっとボタン */}
+        <Center>
+          <Button
+            variant="outline"
+            onClick={handleMoreClick}
+            loading={isSubmitting}
+          >
+            もっと見る
+          </Button>
+        </Center>
 
       </Card>
     </>
